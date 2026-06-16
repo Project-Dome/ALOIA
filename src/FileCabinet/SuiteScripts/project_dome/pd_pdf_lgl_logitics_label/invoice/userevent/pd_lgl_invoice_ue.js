33d0497158756041866b6f015546df25fa692a27
Diego Silva <diego.silva@projectdome.com.br>
2026-06-16 11:50:09 -0300

    feat: "DEV-471 - Personalização de Etiqueta no formulário de INVOICE."

---
+++ src/FileCabinet/SuiteScripts/project_dome/pd_pdf_lgl_logitics_label/invoice/userevent/pd_lgl_invoice_ue.js
@@ -0,0 +1,72 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define([],
+
+    () => {
+        /**
+         * Defines the function definition that is executed before record is loaded.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @param {Form} scriptContext.form - Current form
+         * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
+         * @since 2015.2
+         */
+        const beforeLoad = (scriptContext) => {
+
+            try {
+
+                const {
+                    newRecord,
+                    type,
+                    form
+                } = scriptContext;
+
+                if (type !== scriptContext.UserEventType.VIEW) return null;
+
+                form.clientScriptModulePath = "../client/pd_lgl_invoice_cl";
+
+                addPrintShippingLabelButton(newRecord, form);
+
+            } catch (e) {
+                log.error({
+                    title: "ERROR IN - beforeLoad",
+                    details: {
+                        stack: e.stack,
+                        message: e.message
+                    }
+                });
+            }
+
+        }
+
+        const addPrintShippingLabelButton = (newRecord, form) => {
+
+            try {
+
+                const createdFrom = newRecord.getValue({fieldId: "createdfrom"});
+
+                form.addButton({
+                    id: "custpage_print_shipping_label_btn",
+                    label: "PDF | Print Shipping Label",
+                    functionName: `printShippingLabel(${JSON.stringify(createdFrom)})`
+                });
+
+            } catch (e) {
+                log.error({
+                    title: "ERROR IN - addPrintShippingLabelButton",
+                    details: {
+                        stack: e.stack,
+                        message: e.message
+                    }
+                });
+            }
+
+        }
+
+
+        return {beforeLoad}
+
+    });
---
+++ src/FileCabinet/SuiteScripts/project_dome/pd_pdf_lgl_logitics_label/modules/pd_lgl_contants_module.js
@@ -0,0 +1,19 @@
+/**
+ * @NApiVersion 2.1
+ */
+define([],
+
+    () => {
+
+        return {
+            CUSTOM_SCRIPT: {
+                SUITELET: {
+                    PRINT_LOG_LABEL: {
+                        ID: "customscript_pd_lgl_print_log_label_sl",
+                        DEPLOY: "customdeploy_pd_lgl_print_log_label_sl"
+                    }
+                }
+            }
+        }
+
+    });
