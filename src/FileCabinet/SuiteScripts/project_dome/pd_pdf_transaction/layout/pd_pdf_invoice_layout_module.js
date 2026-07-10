927d0bdc42212f5e4d92cf6a7ff7595cc85331a8
Diego Silva <diego.silva@projectdome.com.br>
2026-07-10 11:10:22 -0300

    fix: "DEV-578 Capacidade de sinais especiais e quebras de linha nos PDFs."

--- src/FileCabinet/SuiteScripts/project_dome/pd_pdf_transaction/layout/pd_pdf_invoice_layout_module.js
+++ src/FileCabinet/SuiteScripts/project_dome/pd_pdf_transaction/layout/pd_pdf_invoice_layout_module.js
@@ -806,7 +806,8 @@ define([
                 .replace(/</g, '&lt;')
                 .replace(/>/g, '&gt;')
                 .replace(/"/g, '&quot;')
-                .replace(/'/g, '&apos;');
+                .replace(/'/g, '&apos;')
+                .replace(/\n/g, '<br/>')
         }
 
         function formatBoxDimensions(str) {
